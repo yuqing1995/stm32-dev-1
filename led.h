9e770e28e55e43e676242824b10d76ec9a68080c
yuqing1995 <yuq1995@gmail.com>
2022-06-26 20:38:22 -0700

    get-green-led-flashing-on-stm32f401

--- led.h
+++ led.h
@@ -8,10 +8,7 @@
 #ifndef LED_H_
 #define LED_H_
 
-#define LED_GREEN  12
-#define LED_ORANGE 13
-#define LED_RED    14
-#define LED_BLUE   15
+#define LED_GREEN  5
 
 #define DELAY_COUNT_1MS 		 1250U
 #define DELAY_COUNT_1S  		(1000U * DELAY_COUNT_1MS)
